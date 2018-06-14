ba148b58ebd4eb4685b9d4bfa1fcac9c6a21f57f
wildman007 <jwild@sent.com>
2018-06-14 09:35:33 +0200

    Added OSC sending of mouse clicks

--- OSCHook/LogMouse.cs
+++ OSCHook/LogMouse.cs
@@ -1,4 +1,5 @@
 ﻿using Gma.System.MouseKeyHook;
+using SharpOSC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,21 +10,43 @@ namespace OSCHook
 {
     class LogMouse
     {
-            public static void Do(Action quit)
-            {
-                Console.WriteLine("Press Q to quit.");
-            Hook.GlobalEvents().MouseClick += LogMouse_MouseClick;
-                //Hook.GlobalEvents().KeyPress += (sender, e) =>
-                //{
-                //    Console.Write(e.KeyChar);
-                //    if (e.KeyChar == 'q') quit();
-                //};
-            }
+        public static UDPSender sender = null;
+        public static String url;
+        public static int port;
+
+        public static void Do(Action quit)
+        {
+        Console.WriteLine("Sending OSC to "+url+" port "+port);
+        sender = new UDPSender(url, port);
+
+        Hook.GlobalEvents().MouseClick += LogMouse_MouseClick;
+
+        }
 
         private static void LogMouse_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
         {
             Console.WriteLine("Mouse: " + e.Button);
-            
+            int btn = 0;
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                btn = 0;
+            }
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                btn = 1;
+            }
+            else if (e.Button == System.Windows.Forms.MouseButtons.Middle)
+            {
+                btn = 2;
+            }
+            else
+            {
+                btn = -1;
+            }
+
+            var message = new OscMessage("/mouse", btn);
+            LogMouse.sender.Send(message);
+            Console.WriteLine("[OSC] Mouse Click: "+btn);
         }
     }
 }
--- OSCHook/Program.cs
+++ OSCHook/Program.cs
@@ -13,9 +13,20 @@ namespace OSCHook
         static void Main(string[] args)
         {
                 var quit = new AutoResetEvent(false);
+            String url = "127.0.0.1";
+            int port = 9000;
+            if (args.Length > 0)
+            {
+                url=args[0];
+            }
+            if (args.Length > 1)
+            {
+                port = int.Parse(args[1]);
+            }
+            LogMouse.url = url;
+            LogMouse.port = port;
 
             Action<Action> action = LogMouse.Do;
-
             action(Application.Exit);
                 Application.Run(new ApplicationContext());
         }

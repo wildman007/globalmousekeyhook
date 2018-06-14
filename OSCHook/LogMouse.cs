d0268e71c6bf4623ea33ee22d13e61e644584529
wildman <jwild@sent.com>
2018-06-14 09:02:16 +0200

    Console Mouse Log

---
+++ OSCHook/LogMouse.cs
@@ -0,0 +1,29 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCHook
+{
+    class LogMouse
+    {
+            public static void Do(Action quit)
+            {
+                Console.WriteLine("Press Q to quit.");
+            Hook.GlobalEvents().MouseClick += LogMouse_MouseClick;
+                //Hook.GlobalEvents().KeyPress += (sender, e) =>
+                //{
+                //    Console.Write(e.KeyChar);
+                //    if (e.KeyChar == 'q') quit();
+                //};
+            }
+
+        private static void LogMouse_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Console.WriteLine("Mouse: " + e.Button);
+            
+        }
+    }
+}
---
+++ OSCHook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSCHook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var quit = new AutoResetEvent(false);
+
+            Action<Action> action = LogMouse.Do;
+
+            action(Application.Exit);
+                Application.Run(new ApplicationContext());
+        }
+
+        private static void Exit(Action quit)
+        {
+            Application.Exit();
+        }
+    }
+}
